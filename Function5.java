87c3a51074e634da770d5f861a418c8c953fe45d
AugusteK-Cyber <70704896+AugusteK-Cyber@users.noreply.github.com>
2020-12-03 18:06:06 -0500

    Add files via upload

---
+++ Function5.java
@@ -0,0 +1,17 @@
+package application;
+
+public class Function5 extends Function{
+	
+	@Override
+	public double fnValue(double x) {
+		if (x==0.0) 
+			return Double.MAX_VALUE;
+		else 
+			return Math.sin(1/x);
+	}
+	
+	public String toString() {
+		return "Sin(1/x)";
+	}
+
+}
